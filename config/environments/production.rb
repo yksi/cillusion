7c27b021714b6c28c5b0bd6594e7e6c5c781e247
vitaliy <vitaliy@mypc.(none)>
2014-02-11 00:32:31 +0200

    1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,21 @@ Illusion::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => "587",
+  :domain => "gmail.com",
+  :authentication => "plain",
+  :user_name => "mailer778@gmail.com",
+  :password => "gfhjkm8712",
+  :enable_starttls_auto => true
+}
 end
