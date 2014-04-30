cdee52b1531a44af11a34e555dcd2b412d9d3702
yksi <embox@ukr.net>
2014-05-01 02:38:51 +0300

    car

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,5 @@ CarrierWave.configure do |config|
     :aws_access_key_id     => 'AKIAJ3XL2P7K6W43SB3A',
     :aws_secret_access_key => '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'
   }
-  config.fog_directory    = 'cillusion'
+  config.fog_directory    = 'cillusion'jnjn
 end
