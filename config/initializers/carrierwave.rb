14b72890b1e62a43b6b706680d8c5a36500c8cfe
yksi <embox@ukr.net>
2014-05-01 02:31:28 +0300

    car

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => ENV['AWS_KEY'],
-    :aws_secret_access_key => ENV['AWS_SECRET']
+    :aws_access_key_id     => 'AKIAJ3XL2P7K6W43SB3A',
+    :aws_secret_access_key => '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'
   }
-  config.fog_directory    = 'path_fo_files'
+  config.fog_directory    = 'cillusion'
 end
