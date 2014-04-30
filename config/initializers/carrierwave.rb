5b8c8a84507eb959e526e26cf3b66762c1a65c17
yksi <embox@ukr.net>
2014-05-01 02:42:50 +0300

    fixed untracked files

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider              => 'AWS',
-    :aws_access_key_id     => 'AKIAJ3XL2P7K6W43SB3A',
-    :aws_secret_access_key => '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'
-  }
-  config.fog_directory    = 'cillusion'
-end
