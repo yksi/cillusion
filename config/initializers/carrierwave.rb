2c6701dd5fa27eb5c8935d2613b7059710e609f5
vitaliy <embox@ukr.net>
2014-05-01 00:03:53 +0300

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => 'AKIAI5LTV7VBJBBUIYVA',
-    :aws_secret_access_key => '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'
+    :aws_access_key_id     => ENV['AWS_KEY'],
+    :aws_secret_access_key => ENV['AWS_SECRET']
   }
-  config.fog_directory    = 'cillusion'
+  config.fog_directory    = 'path_fo_files'
 end
