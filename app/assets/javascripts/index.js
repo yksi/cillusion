24cb79d2458d1caacce6c36cd5aee5dceca66aca
yksi <embox@ukr.net>
2014-05-12 20:34:16 +0300

    reborn

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -60,5 +60,30 @@ $( document ).ready(function() {
 
   })
 
+  $('.navbar-collapse').on('show.bs.collapse', function () {
+    $('.browse-btn').removeClass('rounded-btn').addClass('pander').html($('.browse-btn').html() + '| Browse')
+    $('.timeline-btn').removeClass('rounded-btn').addClass('pander').html($('.timeline-btn').html() + '| Timeline')
+    $('.messages-btn').removeClass('rounded-btn').addClass('pander').html($('.messages-btn').html() + '| Messages')
+    $('.groups-btn').removeClass('rounded-btn').addClass('pander').html($('.groups-btn').html() + '| Groups')
+    $('.new_article-btn').removeClass('rounded-btn').html($('.new_article-btn').html() + '| Create new article').css('padding', '20px')
+    $('.navbar-search').css('display', 'none')
+    $('.nav-btn').removeClass('nav-btn').addClass('nav-btn-removed')
+  })
+
+  $('.navbar-collapse').on('hide.bs.collapse', function () {
+    $('.browse-btn').addClass('rounded-btn').removeClass('pander').html($('.browse-btn').html().split('|')[0])
+    $('.timeline-btn').addClass('rounded-btn').removeClass('pander').html($('.timeline-btn').html().split('|')[0])
+    $('.messages-btn').addClass('rounded-btn').removeClass('pander').html($('.messages-btn').html().split('|')[0])
+    $('.groups-btn').addClass('rounded-btn').removeClass('pander').html($('.groups-btn').html().split('|')[0])
+    $('.new_article-btn').addClass('rounded-btn').html($('.new_article-btn').html().split('|')[0]).css('padding', '')
+
+    $('.navbar-search').css('display', 'inline')
+    $('.nav-btn').addClass('nav-btn').removeClass('nav-btn-removed')
+  })
+
+  $( window ).resize(function() {
+    if($('.navbar-collapse.in').css('display') == 'block')$('.navbar-collapse').collapse('toggle')
+  })
+
 });
 
--- app/controllers/browse_controller.rb
+++ app/controllers/browse_controller.rb
@@ -1,31 +1,21 @@
 class BrowseController < ApplicationController
-  before_action :find_articles
-  before_action :find_users
 
   def index
     @comments = Comment.order(created_at: :desc)
   end
 
+
   def timeline
+    @logs = Log.order(created_at: :desc)
     current_user.read_news!
   end
 
   def articles
-    @articles = Article.search(params[:search]).paginate(page: params[:page], per_page: 10)
+    @articles = Article.search(params[:search]).paginate(page: params[:page], per_page: 9)
   end
 
   def users
-    @users = User.search(params[:search]).paginate(page: params[:page], per_page: 10)
-  end
-
-  private
-
-  def find_articles
-    @articles = Article.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
-  end
-
-  def find_users
-    @users = User.order(created_at: :desc)
+    @users = User.search(params[:search]).paginate(page: params[:page], per_page: 9)
   end
 
 end
