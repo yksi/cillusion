698862e011b4028aee98b21ac7cb0273c6fc4337
vitaliy <embox@ukr.net>
2015-03-13 14:47:15 +0200

    Update article_photo_uploader.rb

--- app/uploaders/article_photo_uploader.rb
+++ app/uploaders/article_photo_uploader.rb
@@ -3,7 +3,7 @@
 class ArticlePhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  storage :fog
+  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
